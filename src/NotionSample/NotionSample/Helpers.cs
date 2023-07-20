c2cbbd033cfaabd65aac17e6f90a79a13638f0a9
Jung Hyun, Nam <rkttu@rkttu.com>
2023-07-21 00:21:25 +0900

    Refactor entrypoint codes
    - Add NotionV1Client class
    - Split API key load procedures into new Helper class

---
+++ src/NotionSample/NotionSample/Helpers.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NotionSample;
+
+internal static class Helpers
+{
+    private static readonly string NotionApiKeyFileName = "notion_api_key.txt";
+
+    public static async Task<string?> LoadApiKeyFromUserDirectory(
+        string? apiKeyFileName = default,
+        CancellationToken cancellationToken = default)
+    {
+        if (string.IsNullOrWhiteSpace(apiKeyFileName))
+            apiKeyFileName = NotionApiKeyFileName;
+
+        var keyFilePath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+            apiKeyFileName);
+
+        var notionApiFileContent = await File.ReadAllTextAsync(
+            keyFilePath, new UTF8Encoding(false),
+            cancellationToken);
+
+        var secretKeyMatch = Regex.Match(
+            notionApiFileContent, "secret_.+", RegexOptions.Compiled);
+
+        if (secretKeyMatch.Success)
+            return secretKeyMatch.Value;
+
+        return default;
+    }
+}
---
+++ src/NotionSample/NotionSample/NotionV1Client.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using NotionSample.Models.Page;
+
+namespace NotionSample;
+
+public sealed class NotionV1Client
+{
+    public NotionV1Client(string notionApiKey, string? notionApiVersion = default)
+    {
+        var client = new HttpClient();
+        client.BaseAddress = new Uri(
+            $"https://api.notion.com/v1/");
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", notionApiKey);
+
+        if (!string.IsNullOrWhiteSpace(notionApiVersion))
+            client.DefaultRequestHeaders.Add("Notion-Version", notionApiVersion);
+
+        this.HttpClient = client;
+    }
+
+    public NotionV1Client(HttpClient httpClient)
+    {
+        this.HttpClient = httpClient;
+    }
+
+    public HttpClient HttpClient { get; private set; }
+
+    public async Task<NotionPageObject?> FetchNotionPage(
+        string targetPageId,
+        CancellationToken cancellationToken = default)
+    {
+        var elem = await this.HttpClient.GetFromJsonAsync<JsonElement>(
+            $"pages/{targetPageId}",
+            cancellationToken)
+            .ConfigureAwait(false);
+
+        if (elem.TryGetProperty("object", out JsonElement elem2) &&
+            string.Equals(elem2.GetString(), "page", StringComparison.Ordinal))
+            return new NotionPageObject(elem);
+
+        return default;
+    }
+}
