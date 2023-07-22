50c543f6c95e6a69b8000b29180aed4bb59564a9
Jung Hyun, Nam <rkttu@rkttu.com>
2023-07-22 19:34:56 +0900

    Move and pre-create type map tuple array to dictionary

--- src/NotionSample/NotionSample/Models/NotionWrappedObjectExtensions.cs
+++ src/NotionSample/NotionSample/Models/NotionWrappedObjectExtensions.cs
@@ -31,28 +31,28 @@ internal static class NotionWrappedObjectExtensions
     }
 
     public static INotionTypedObject? CreateNotionTypedObjectInstance(
-        this JsonElement elem, params Tuple<string, Type>[] map)
+        this JsonElement elem, IDictionary<string, Type> map)
     {
         var type = elem.GetProperty("type").GetString();
 
-        for (var i = 0; i < map.Length; i++)
+        foreach (var eachPair in map)
         {
-            if (string.Equals(map[i].Item1, type, StringComparison.Ordinal))
-                return Activator.CreateInstance(map[i].Item2, elem) as INotionTypedObject;
+            if (string.Equals(eachPair.Key, type, StringComparison.Ordinal))
+                return Activator.CreateInstance(eachPair.Value, elem) as INotionTypedObject;
         }
 
         return default;
     }
 
     public static INotionRichTextObject? CreateNotionTypedRichTextObjectInstance(
-        this JsonElement elem, params Tuple<string, Type>[] map)
+        this JsonElement elem, IDictionary<string, Type> map)
     {
         var type = elem.GetProperty("type").GetString();
 
-        for (var i = 0; i < map.Length; i++)
+        foreach (var eachPair in map)
         {
-            if (string.Equals(map[i].Item1, type, StringComparison.Ordinal))
-                return Activator.CreateInstance(map[i].Item2, elem) as INotionRichTextObject;
+            if (string.Equals(eachPair.Key, type, StringComparison.Ordinal))
+                return Activator.CreateInstance(eachPair.Value, elem) as INotionRichTextObject;
         }
 
         return default;
@@ -90,140 +90,152 @@ internal static class NotionWrappedObjectExtensions
         return list.AsReadOnly();
     }
 
-    public static INotionBlockObject? CreateNotionBlockObject(
-        this JsonElement elem) =>
-        elem.CreateNotionTypedObjectInstance(
-            new[]
-            {
-                new Tuple<string, Type>("bookmark", typeof(NotionBookmarkBlockObject)),
-                new Tuple<string, Type>("breadcrumb", typeof(NotionBreadcrumbBlockObject)),
-                new Tuple<string, Type>("bulleted_list_item", typeof(NotionBulletedListItemBlockObject)),
-                new Tuple<string, Type>("callout", typeof(NotionCalloutBlockObject)),
-                new Tuple<string, Type>("child_database", typeof(NotionChildDatabaseBlockObject)),
-                new Tuple<string, Type>("child_page", typeof(NotionChildPageBlockObject)),
-                new Tuple<string, Type>("column", typeof(NotionColumnBlockObject)),
-                new Tuple<string, Type>("column_list", typeof(NotionColumnListBlockObject)),
-                new Tuple<string, Type>("divider", typeof(NotionDividerBlockObject)),
-                new Tuple<string, Type>("embed", typeof(NotionEmbedBlockObject)),
-                new Tuple<string, Type>("equation", typeof(NotionEquationBlockObject)),
-                new Tuple<string, Type>("file", typeof(NotionFileBlockObject)),
-                new Tuple<string, Type>("heading_1", typeof(NotionHeadingLv1BlockObject)),
-                new Tuple<string, Type>("heading_2", typeof(NotionHeadingLv2BlockObject)),
-                new Tuple<string, Type>("heading_3", typeof(NotionHeadingLv3BlockObject)),
-                new Tuple<string, Type>("image", typeof(NotionImageBlockObject)),
-                new Tuple<string, Type>("link_to_page", typeof(NotionLinkToPageBlockObject)),
-                new Tuple<string, Type>("numbered_list_item", typeof(NotionNumberedListItemBlockObject)),
-                new Tuple<string, Type>("paragraph", typeof(NotionParagraphBlockObject)),
-                new Tuple<string, Type>("pdf", typeof(NotionPdfBlockObject)),
-                new Tuple<string, Type>("quote", typeof(NotionQuoteBlockObject)),
-                new Tuple<string, Type>("synced_block", typeof(NotionSyncedBlockObject)),
-                new Tuple<string, Type>("table", typeof(NotionTableBlockObject)),
-                new Tuple<string, Type>("table_of_contents", typeof(NotionTableOfContentsBlockObject)),
-                new Tuple<string, Type>("table_row", typeof(NotionTableRowBlockObject)),
-                new Tuple<string, Type>("template", typeof(NotionTemplateBlockObject)),
-                new Tuple<string, Type>("to_do", typeof(NotionToDoBlockObject)),
-                new Tuple<string, Type>("toggle", typeof(NotionToggleBlockObject)),
-                new Tuple<string, Type>("unsupported", typeof(NotionUnsupportedBlockObject)),
-                new Tuple<string, Type>("video", typeof(NotionVideoBlockObject)),
-                new Tuple<string, Type>("database", typeof(NotionDatabaseMentionBlockObject)),
-                new Tuple<string, Type>("date", typeof(NotionDateMentionBlockObject)),
-                new Tuple<string, Type>("link_preview", typeof(NotionLinkPreviewMentionBlockObject)),
-                new Tuple<string, Type>("page", typeof(NotionPageMentionBlockObject)),
-                new Tuple<string, Type>("user", typeof(NotionUserMentionBlockObject)),
-            }) as INotionBlockObject;
+    private static readonly IDictionary<string, Type> BlockObjectTypes =
+        new ReadOnlyDictionary<string, Type>(new Dictionary<string, Type>()
+        {
+            { "bookmark", typeof(NotionBookmarkBlockObject) },
+            { "breadcrumb", typeof(NotionBreadcrumbBlockObject) },
+            { "bulleted_list_item", typeof(NotionBulletedListItemBlockObject) },
+            { "callout", typeof(NotionCalloutBlockObject) },
+            { "child_database", typeof(NotionChildDatabaseBlockObject) },
+            { "child_page", typeof(NotionChildPageBlockObject) },
+            { "column", typeof(NotionColumnBlockObject) },
+            { "column_list", typeof(NotionColumnListBlockObject) },
+            { "divider", typeof(NotionDividerBlockObject) },
+            { "embed", typeof(NotionEmbedBlockObject) },
+            { "equation", typeof(NotionEquationBlockObject) },
+            { "file", typeof(NotionFileBlockObject) },
+            { "heading_1", typeof(NotionHeadingLv1BlockObject) },
+            { "heading_2", typeof(NotionHeadingLv2BlockObject) },
+            { "heading_3", typeof(NotionHeadingLv3BlockObject) },
+            { "image", typeof(NotionImageBlockObject) },
+            { "link_to_page", typeof(NotionLinkToPageBlockObject) },
+            { "numbered_list_item", typeof(NotionNumberedListItemBlockObject) },
+            { "paragraph", typeof(NotionParagraphBlockObject) },
+            { "pdf", typeof(NotionPdfBlockObject) },
+            { "quote", typeof(NotionQuoteBlockObject) },
+            { "synced_block", typeof(NotionSyncedBlockObject) },
+            { "table", typeof(NotionTableBlockObject) },
+            { "table_of_contents", typeof(NotionTableOfContentsBlockObject) },
+            { "table_row", typeof(NotionTableRowBlockObject) },
+            { "template", typeof(NotionTemplateBlockObject) },
+            { "to_do", typeof(NotionToDoBlockObject) },
+            { "toggle", typeof(NotionToggleBlockObject) },
+            { "unsupported", typeof(NotionUnsupportedBlockObject) },
+            { "video", typeof(NotionVideoBlockObject) },
+            { "database", typeof(NotionDatabaseMentionBlockObject) },
+            { "date", typeof(NotionDateMentionBlockObject) },
+            { "link_preview", typeof(NotionLinkPreviewMentionBlockObject) },
+            { "page", typeof(NotionPageMentionBlockObject) },
+            { "user", typeof(NotionUserMentionBlockObject) }
+        });
 
-    public static INotionPropertyValueObject? CreateNotionPropertyValueObject(
-        this JsonElement elem) =>
-        elem.CreateNotionTypedObjectInstance(
-            new[]
-            {
-                new Tuple<string, Type>("date", typeof(NotionDatePropertyValueObject)),
-                new Tuple<string, Type>("title", typeof(NotionTitlePropertyValueObject)),
-                new Tuple<string, Type>("rich_text", typeof(NotionRichTextPropertyValueObject)),
-                new Tuple<string, Type>("number", typeof(NotionNumberPropertyValueObject)),
-                new Tuple<string, Type>("select", typeof(NotionSelectPropertyValueObject)),
-                new Tuple<string, Type>("multi_select", typeof(NotionMultiSelectPropertyValueObject)),
-                new Tuple<string, Type>("status", typeof(NotionStatusPropertyValueObject)),
-                new Tuple<string, Type>("formula", typeof(NotionFormulaPropertyValueObject)),
-                new Tuple<string, Type>("relation", typeof(NotionRelationPropertyValueObject)),
-                new Tuple<string, Type>("rollup", typeof(NotionRollupPropertyValueObject)),
-                new Tuple<string, Type>("people", typeof(NotionPeoplePropertyValueObject)),
-                new Tuple<string, Type>("files", typeof(NotionFilesPropertyValueObject)),
-                new Tuple<string, Type>("checkbox", typeof(NotionCheckBoxPropertyValueObject)),
-                new Tuple<string, Type>("url", typeof(NotionUrlPropertyValueObject)),
-                new Tuple<string, Type>("email", typeof(NotionEmailPropertyValueObject)),
-                new Tuple<string, Type>("phone_number", typeof(NotionPhoneNumberPropertyValueObject)),
-                new Tuple<string, Type>("created_time", typeof(NotionCreatedTimePropertyValueObject)),
-                new Tuple<string, Type>("created_by", typeof(NotionCreatedByPropertyValueObject)),
-                new Tuple<string, Type>("last_edited_time", typeof(NotionLastEditedTimePropertyValueObject)),
-                new Tuple<string, Type>("last_edited_by", typeof(NotionLastEditedByPropertyValueObject)),
-            }) as INotionPropertyValueObject;
+    public static INotionBlockObject? CreateNotionBlockObject(this JsonElement elem) =>
+        elem.CreateNotionTypedObjectInstance(BlockObjectTypes) as INotionBlockObject;
 
-    public static INotionRichTextObject? CreateNotionRichTextObject(
-        this JsonElement elem) =>
-        elem.CreateNotionTypedRichTextObjectInstance(
-            new[]
-            {
-                new Tuple<string, Type>("text", typeof(NotionTextRichTextObject)),
-                new Tuple<string, Type>("mention", typeof(NotionMentionRichTextObject)),
-                new Tuple<string, Type>("equation", typeof(NotionEquationRichTextObject)),
-            });
+    private static readonly IDictionary<string, Type> PropertyValueObjectTypes =
+        new ReadOnlyDictionary<string, Type>(new Dictionary<string, Type>()
+        {
+            { "date", typeof(NotionDatePropertyValueObject) },
+            { "title", typeof(NotionTitlePropertyValueObject) },
+            { "rich_text", typeof(NotionRichTextPropertyValueObject) },
+            { "number", typeof(NotionNumberPropertyValueObject) },
+            { "select", typeof(NotionSelectPropertyValueObject) },
+            { "multi_select", typeof(NotionMultiSelectPropertyValueObject) },
+            { "status", typeof(NotionStatusPropertyValueObject) },
+            { "formula", typeof(NotionFormulaPropertyValueObject) },
+            { "relation", typeof(NotionRelationPropertyValueObject) },
+            { "rollup", typeof(NotionRollupPropertyValueObject) },
+            { "people", typeof(NotionPeoplePropertyValueObject) },
+            { "files", typeof(NotionFilesPropertyValueObject) },
+            { "checkbox", typeof(NotionCheckBoxPropertyValueObject) },
+            { "url", typeof(NotionUrlPropertyValueObject) },
+            { "email", typeof(NotionEmailPropertyValueObject) },
+            { "phone_number", typeof(NotionPhoneNumberPropertyValueObject) },
+            { "created_time", typeof(NotionCreatedTimePropertyValueObject) },
+            { "created_by", typeof(NotionCreatedByPropertyValueObject) },
+            { "last_edited_time", typeof(NotionLastEditedTimePropertyValueObject) },
+            { "last_edited_by", typeof(NotionLastEditedByPropertyValueObject) }
+        });
 
-    public static INotionMentionObject? CreateNotionMentionObject(
-        this JsonElement elem) =>
-        elem.CreateNotionTypedObjectInstance(
-            new[]
-            {
-                new Tuple<string, Type>("database", typeof(NotionDatabaseMentionObject)),
-                new Tuple<string, Type>("date", typeof(NotionDateMentionObject)),
-                new Tuple<string, Type>("link_preview", typeof(NotionLinkPreviewMentionObject)),
-                new Tuple<string, Type>("page", typeof(NotionPageMentionObject)),
-                new Tuple<string, Type>("template_mention", typeof(NotionTemplateMentionObject)),
-                new Tuple<string, Type>("user", typeof(NotionUserMentionObject)),
-            }) as INotionMentionObject;
+    public static INotionPropertyValueObject? CreateNotionPropertyValueObject(this JsonElement elem) =>
+        elem.CreateNotionTypedObjectInstance(PropertyValueObjectTypes) as INotionPropertyValueObject;
 
-    public static INotionTemplateMentionObject? CreateNotionTemplateMentionObject(
-        this JsonElement elem) =>
-        elem.CreateNotionTypedObjectInstance(
-            new[]
-            {
-                new Tuple<string, Type>("template_mention_date", typeof(NotionTemplateMentionDateObject)),
-                new Tuple<string, Type>("template_mention_date", typeof(NotionTemplateMentionUserObject)),
-            }) as INotionTemplateMentionObject;
+    private static readonly IDictionary<string, Type> RichTextObjectTypes =
+        new ReadOnlyDictionary<string, Type>(new Dictionary<string, Type>()
+        {
+            { "text", typeof(NotionTextRichTextObject) },
+            { "mention", typeof(NotionMentionRichTextObject) },
+            { "equation", typeof(NotionEquationRichTextObject) }
+        });
 
-    public static INotionUserObject? CreateNotionUserObject(
-        this JsonElement elem) =>
-        elem.CreateNotionTypedObjectInstance(
-            new[]
-            {
-                new Tuple<string, Type>("person", typeof(NotionPersonUserObject)),
-                new Tuple<string, Type>("bot", typeof(NotionBotUserObject)),
-            }) as INotionUserObject;
+    public static INotionRichTextObject? CreateNotionRichTextObject(this JsonElement elem) =>
+        elem.CreateNotionTypedRichTextObjectInstance(RichTextObjectTypes);
 
-    public static INotionTypedObject? CreateParentNotionObject(
-        this JsonElement elem) =>
-        elem.CreateNotionTypedObjectInstance(
-            new[] {
-                new Tuple<string, Type>("database_id", typeof(NotionDatabaseParentObject)),
-                new Tuple<string, Type>("page_id", typeof(NotionPageParentObject)),
-                new Tuple<string, Type>("workspace", typeof(NotionWorkspaceParentObject)),
-                new Tuple<string, Type>("block_id", typeof(NotionBlockParentObject)),
-            });
+    private static readonly IDictionary<string, Type> MentionObjectTypes =
+        new ReadOnlyDictionary<string, Type>(new Dictionary<string, Type>()
+        {
+            { "database", typeof(NotionDatabaseMentionObject) },
+            { "date", typeof(NotionDateMentionObject) },
+            { "link_preview", typeof(NotionLinkPreviewMentionObject) },
+            { "page", typeof(NotionPageMentionObject) },
+            { "template_mention", typeof(NotionTemplateMentionObject) },
+            { "user", typeof(NotionUserMentionObject) }
+        });
 
-    public static INotionTypedObject? CreateFileExternalNotionObject(
-        this JsonElement elem) =>
-        elem.CreateNotionTypedObjectInstance(
-            new[] {
-                new Tuple<string, Type>("file", typeof(NotionFileObject)),
-                new Tuple<string, Type>("external", typeof(NotionExternalObject)),
-            });
+    public static INotionMentionObject? CreateNotionMentionObject(this JsonElement elem) =>
+        elem.CreateNotionTypedObjectInstance(MentionObjectTypes) as INotionMentionObject;
 
-    public static INotionTypedObject? CreateFileExternalEmojiNotionObject(
-        this JsonElement elem) =>
-        elem.CreateNotionTypedObjectInstance(
-            new[] {
-                new Tuple<string, Type>("file", typeof(NotionFileObject)),
-                new Tuple<string, Type>("external", typeof(NotionExternalObject)),
-                new Tuple<string, Type>("emoji", typeof(NotionEmojiObject)),
-            });
+    private static readonly IDictionary<string, Type> TemplateMentionObjectTypes =
+        new ReadOnlyDictionary<string, Type>(new Dictionary<string, Type>()
+        {
+            { "template_mention_date", typeof(NotionTemplateMentionDateObject) },
+            { "template_mention_user", typeof(NotionTemplateMentionUserObject) }
+        });
+
+    public static INotionTemplateMentionObject? CreateNotionTemplateMentionObject(this JsonElement elem) =>
+        elem.CreateNotionTypedObjectInstance(TemplateMentionObjectTypes) as INotionTemplateMentionObject;
+
+    private static readonly IDictionary<string, Type> UserObjectTypes =
+        new ReadOnlyDictionary<string, Type>(new Dictionary<string, Type>()
+        {
+            { "person", typeof(NotionPersonUserObject) },
+            { "bot", typeof(NotionBotUserObject) }
+        });
+
+    public static INotionUserObject? CreateNotionUserObject(this JsonElement elem) =>
+        elem.CreateNotionTypedObjectInstance(UserObjectTypes) as INotionUserObject;
+
+    private static readonly IDictionary<string, Type> ParentObjectTypes =
+        new ReadOnlyDictionary<string, Type>(new Dictionary<string, Type>()
+        {
+            { "database_id", typeof(NotionDatabaseParentObject) },
+            { "page_id", typeof(NotionPageParentObject) },
+            { "workspace", typeof(NotionWorkspaceParentObject) },
+            { "block_id", typeof(NotionBlockParentObject) }
+        });
+
+    public static INotionTypedObject? CreateParentNotionObject(this JsonElement elem) =>
+        elem.CreateNotionTypedObjectInstance(ParentObjectTypes);
+
+    private static readonly IDictionary<string, Type> FileExternalObjectTypes =
+        new ReadOnlyDictionary<string, Type>(new Dictionary<string, Type>()
+        {
+            { "file", typeof(NotionFileObject) },
+            { "external", typeof(NotionExternalObject) }
+        });
+
+    public static INotionTypedObject? CreateFileExternalNotionObject(this JsonElement elem) =>
+        elem.CreateNotionTypedObjectInstance(FileExternalObjectTypes);
+
+    private static readonly IDictionary<string, Type> FileExternalEmojiObjectTypes =
+        new ReadOnlyDictionary<string, Type>(new Dictionary<string, Type>()
+        {
+            { "file", typeof(NotionFileObject) },
+            { "external", typeof(NotionExternalObject) },
+            { "emoji", typeof(NotionEmojiObject) }
+        });
+
+    public static INotionTypedObject? CreateFileExternalEmojiNotionObject(this JsonElement elem) =>
+        elem.CreateNotionTypedObjectInstance(FileExternalEmojiObjectTypes);
 }
