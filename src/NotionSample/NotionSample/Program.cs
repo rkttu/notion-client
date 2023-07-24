c62e7decd7bb9d9700cf83724c9e2cb34168aced
Jung Hyun, Nam <rkttu@rkttu.com>
2023-07-24 20:12:27 +0900

    Add text render sample code

--- src/NotionSample/NotionSample/Program.cs
+++ src/NotionSample/NotionSample/Program.cs
@@ -1,4 +1,6 @@
-﻿using NotionSample.Models.Contracts;
+﻿using NotionSample.Models.Block;
+using NotionSample.Models.Comment;
+using NotionSample.Models.Contracts;
 using System;
 using System.Runtime.CompilerServices;
 using System.Threading;
@@ -36,8 +38,67 @@ public sealed class Program
 
         await foreach (var eachChildBlock in notion.FetchAllChildNotionBlocks(targetPageId))
         {
-            await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {eachChildBlock?.Id}");
+            switch (eachChildBlock)
+            {
+                case NotionParagraphBlockObject p:
+                    await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {string.Concat(p.Text.Select(x => x?.PlainText))}");
+                    break;
+
+                case NotionBulletedListItemBlockObject p:
+                    await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {string.Concat(p.Text.Select(x => x?.PlainText))}");
+                    break;
+
+                case NotionNumberedListItemBlockObject p:
+                    await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {string.Concat(p.Text.Select(x => x?.PlainText))}");
+                    break;
+
+                case NotionCodeBlockObject p:
+                    await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {string.Concat(p.Text.Select(x => x?.PlainText))}");
+                    break;
+
+                case NotionHeadingLv1BlockObject p:
+                    await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {string.Concat(p.Text.Select(x => x?.PlainText))}");
+                    break;
+
+                case NotionHeadingLv2BlockObject p:
+                    await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {string.Concat(p.Text.Select(x => x?.PlainText))}");
+                    break;
+
+                case NotionHeadingLv3BlockObject p:
+                    await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {string.Concat(p.Text.Select(x => x?.PlainText))}");
+                    break;
+
+                case NotionQuoteBlockObject p:
+                    await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {string.Concat(p.Text.Select(x => x?.PlainText))}");
+                    break;
+
+                case NotionTemplateBlockObject p:
+                    await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {string.Concat(p.Text.Select(x => x?.PlainText))}");
+                    break;
+
+                case NotionToDoBlockObject p:
+                    await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {string.Concat(p.Text.Select(x => x?.PlainText))}");
+                    break;
+
+                case NotionToggleBlockObject p:
+                    await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {string.Concat(p.Text.Select(x => x?.PlainText))}");
+                    break;
+
+                case NotionBookmarkBlockObject p:
+                    await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {p.Url}");
+                    break;
+
+                case NotionEmbedBlockObject p:
+                    await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]: {p.Url}");
+                    break;
+
+                default:
+                    //await Console.Out.WriteLineAsync($"[{eachChildBlock?.GetType().Name}]");
+                    break;
+            }
         }
+
+        return;
     }
 
 }
